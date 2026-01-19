d6ebd204423f86d6d84e0ea6e91e370476757a65
Udor Blessing <blessingworld007@gmail.com>
2026-01-19 09:46:22 -0700

    update function to working version

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,44 +1,68 @@
-import json
 import boto3
 import logging
-import os
+import json
 import jwt
 from datetime import datetime, timedelta, timezone
 from jwt import ExpiredSignatureError, InvalidTokenError
-from dotenv import load_dotenv
-load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
-create_event = {'ClientRequestToken': '9454be8a-9c5c-4bc6-80d9-79bbc8d3a78b', 
-                'RotationToken': 'a08fc217-19db-461b-8bf4-a21cf05eba23', 
-                'SecretId': 'arn:aws:secretsmanager:ca-central-1:640351538571:secret:prod/analytics-8m7QBb', 
-                'Step': 'createSecret'}
+SECRET_KEY = "a-string-secret-at-least-256-bits-long" #this should be changed and stored securely
+ALGORITHM = 'HS256'
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-set_event = {'ClientRequestToken': '9454be8a-9c5c-4bc6-80d9-79bbc8d3a78b', 
-              'RotationToken': '0b1fde39-60d1-4fb0-b96d-e35f7bab63d5', 
-              'SecretId': 'arn:aws:secretsmanager:ca-central-1:640351538571:secret:prod/analytics-8m7QBb', 
-              'Step': 'setSecret'}
+def lambda_handler(event, context):
+    
+    logger.info(f" event {event}")
 
-test_event = {'ClientRequestToken': '9454be8a-9c5c-4bc6-80d9-79bbc8d3a78b', 
-               'RotationToken': '2b774b9d-4919-4df9-8ab1-bf3a505e4848', 
-               'SecretId': 'arn:aws:secretsmanager:ca-central-1:640351538571:secret:prod/analytics-8m7QBb', 
-               'Step': 'testSecret'}
+    arn = event['SecretId']
+    token = event['ClientRequestToken']
+    step = event['Step']
+    
+    # Setup the client
+    service_client = boto3.client('secretsmanager')
 
-finish_event = {'ClientRequestToken': '9454be8a-9c5c-4bc6-80d9-79bbc8d3a78b', 
-                 'RotationToken': 'fb3d58b8-03b9-4de2-b4e1-3b379b639b2e', 
-                 'SecretId': 'arn:aws:secretsmanager:ca-central-1:640351538571:secret:prod/analytics-8m7QBb', 
-                 'Step': 'finishSecret'}
+    # Make sure the version is staged correctly
+    metadata = service_client.describe_secret(SecretId=arn)
+    
+    if not metadata['RotationEnabled']:
+        logger.error("Secret %s is not enabled for rotation" % arn)
+        raise ValueError("Secret %s is not enabled for rotation" % arn)
+    
+    # Get versions
+    versions = metadata['VersionIdsToStages']
 
+    if token not in versions:
+        logger.error("Secret version %s has no stage for rotation of secret %s." % (token, arn))
+        raise ValueError("Secret version %s has no stage for rotation of secret %s." % (token, arn))
+    
+    if "AWSCURRENT" in versions[token]:
+        logger.info("Secret version %s already set as AWSCURRENT for secret %s." % (token, arn))
+        return
+    
+    elif "AWSPENDING" not in versions[token]:
+        logger.error("Secret version %s not set as AWSPENDING for rotation of secret %s." % (token, arn))
+        raise ValueError("Secret version %s not set as AWSPENDING for rotation of secret %s." % (token, arn))
+    
+    
+    
+    if step == "createSecret":
+        create_secret(service_client, arn, token)
+
+    elif step == "setSecret":
+        set_secret(service_client, arn, token)
+
+    elif step == "testSecret":
+        test_secret(service_client, arn, token)
+
+    elif step == "finishSecret":
+        finish_secret(service_client, arn, token)
+
+    else:
+        raise ValueError("Invalid step parameter")
+ 
+ 
 def create_secret(service_client, arn, token):
-    logger.info(f"\n== Create Secret Triggered ==\n {token}")
-    # Make sure the current secret exists
-    service_client.get_secret_value(SecretId=arn, VersionStage="AWSCURRENT")
 
     try:
         
@@ -46,9 +70,7 @@ def create_secret(service_client, arn, token):
         service_client.get_secret_value(SecretId=arn, VersionId=token, VersionStage="AWSPENDING")
         logger.info("createSecret: Successfully retrieved secret for %s." % arn)
     except service_client.exceptions.ResourceNotFoundException:
-        logger.error(
-            'Not able to get secret'
-        )
+
         user_data = {
             "user_id": "177f5a63-1497-4808-a315-a7d90683df14",
             "role": "developer"
@@ -56,13 +78,21 @@ def create_secret(service_client, arn, token):
         
         new_token = generate_jwt_token(user_data, SECRET_KEY)
         # Put the secret
-        service_client.put_secret_value(SecretId=arn, ClientRequestToken=token, SecretString=new_token, VersionStages=['AWSPENDING'])
+        # third_party_service_key should be replaced with the actual key name used in secrets manager
+        service_client.put_secret_value(
+            SecretId=arn, 
+            ClientRequestToken=token, 
+            SecretString=json.dumps({
+                "third_party_service_key": new_token
+            }),
+            VersionStages=['AWSPENDING'])
+        
         logger.info("createSecret: Successfully put secret for ARN %s and version %s." % (arn, token))
 
 def generate_jwt_token(payload, secret_key, algorithm="HS256"):
 
     now = datetime.now(timezone.utc)
-    expiry = now - timedelta(days=30)
+    expiry = now + timedelta(days=30)
 
     token_payload = {
         **payload,
@@ -71,13 +101,12 @@ def generate_jwt_token(payload, secret_key, algorithm="HS256"):
     }
 
     token = jwt.encode(token_payload, secret_key, algorithm=algorithm)
-    print(token)
     return token
 
 
 def set_secret(service_client, arn, token):
     # This is where the secret should be set in the service
-    raise NotImplementedError
+    pass
 
 
 def test_secret(service_client, arn, token):
@@ -88,7 +117,8 @@ def test_secret(service_client, arn, token):
             VersionId=token, 
             VersionStage="AWSPENDING"
         )
-        jwt_token = response['SecretString']
+        parsed_result = json.loads(response['SecretString'])
+        jwt_token = parsed_result['third_party_service_key']
         logger.info("testSecret: Successfully retrieved secret for ARN %s and version %s." % (arn, token))
         
         # Validate the JWT token
@@ -137,58 +167,10 @@ def finish_secret(service_client, arn, token):
             break
 
     # Finalize by staging the secret version current
-    service_client.update_secret_version_stage(SecretId=arn, VersionStage="AWSCURRENT", MoveToVersionId=token, RemoveFromVersionId=current_version)
+    service_client.update_secret_version_stage(SecretId=arn, 
+                                               VersionStage="AWSCURRENT", 
+                                               MoveToVersionId=token, 
+                                               RemoveFromVersionId=current_version)
     logger.info("finishSecret: Successfully set AWSCURRENT stage to version %s for secret %s." % (token, arn))
     
 
-def lambda_handler(event, context):
-    
-    logger.info(f" event {event}")
-
-    arn = event['SecretId']
-    token = event['ClientRequestToken']
-    step = event['Step']
-    
-    # Setup the client
-    service_client = boto3.client('secretsmanager')
-
-    # Make sure the version is staged correctly
-    metadata = service_client.describe_secret(SecretId=arn)
-    
-    if not metadata['RotationEnabled']:
-        logger.error("Secret %s is not enabled for rotation" % arn)
-        raise ValueError("Secret %s is not enabled for rotation" % arn)
-    
-    versions = metadata['VersionIdsToStages']
-    print(versions, f"tokn : {token}")
-    if token not in versions:
-        logger.error("Secret version %s has no stage for rotation of secret %s." % (token, arn))
-        raise ValueError("Secret version %s has no stage for rotation of secret %s." % (token, arn))
-    
-    if "AWSCURRENT" in versions[token]:
-        logger.info("Secret version %s already set as AWSCURRENT for secret %s." % (token, arn))
-        return
-    
-    elif "AWSPENDING" not in versions[token]:
-        logger.error("Secret version %s not set as AWSPENDING for rotation of secret %s." % (token, arn))
-        raise ValueError("Secret version %s not set as AWSPENDING for rotation of secret %s." % (token, arn))
-    
-    if step == "createSecret":
-        create_secret(service_client, arn, token)
-
-    elif step == "setSecret":
-        set_secret(service_client, arn, token)
-
-    elif step == "testSecret":
-        test_secret(service_client, arn, token)
-
-    elif step == "finishSecret":
-        finish_secret(service_client, arn, token)
-
-    else:
-        raise ValueError("Invalid step parameter")
-    
-lambda_handler(finish_event,{})
-lambda_handler(test_event,{})
-lambda_handler(set_event,{})
-lambda_handler(create_event,{})
